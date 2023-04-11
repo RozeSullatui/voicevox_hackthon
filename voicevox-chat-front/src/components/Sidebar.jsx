aabdd57c4fc9cdb147aa81d771fa6b7abe09a8ef
yusei53 <yusei530922@gmail.com>
2023-04-11 17:07:47 +0000

    make_simple_page

---
+++ voicevox-chat-front/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles.css'
+
+export function Sidebar() {
+  
+  return (
+    <div className="sidebar">
+      <h2>People in Chat</h2>
+      <ul>
+        <li>John Doe</li>
+        <li>Jane Doe</li>
+        <li>Bob Smith</li>
+      </ul>
+      {/* 参加者の一覧を表示する */}
+      {/* 例: <div className="participant">User A</div> */}
+    </div>
+  );
+}
--- voicevox-chat-front/src/index.js
+++ voicevox-chat-front/src/index.js
@@ -1,15 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
