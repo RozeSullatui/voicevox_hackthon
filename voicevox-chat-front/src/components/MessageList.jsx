b6024df0837db3418c7c3346e1b344ef9bb008cd
yusei53 <yusei530922@gmail.com>
2023-04-12 02:37:27 +0000

    add

--- voicevox-chat-front/src/components/MessageList.jsx
+++ voicevox-chat-front/src/components/MessageList.jsx
@@ -4,20 +4,15 @@ import '../styles.css';
 
 export const MessageList = ({ messages }) => {
   const [textMessages, setTextMessages] = useState([]);
-  const [showAutoReply, setShowAutoReply] = useState(false);
 
   const handleSendMessage = (text) => {
     if (text !== '') {
-      const newMessage = { text };
+      const newMessage = { text, isUser: true }; // ユーザーが送信したメッセージにisUserプロパティを追加
       setTextMessages((prevMessages) => [...prevMessages, newMessage]);
 
       setTimeout(() => {
-        const autoReply = { text: 'Hello world!' };
-        setShowAutoReply(true);
+        const autoReply = { text: 'Hello world!', isUser: false }; // 自動返信メッセージにisUserプロパティを追加
         setTextMessages((prevMessages) => [...prevMessages, autoReply]);
-        setTimeout(() => {
-          setShowAutoReply(false);
-        }, 1000);
       }, 1000);
     }
   };
@@ -25,14 +20,23 @@ export const MessageList = ({ messages }) => {
   return (
     <div className="message-list">
       {messages.map((message, index) => (
-        <Message key={index} message={message} className="right" />
+        <Message
+          key={index}
+          message={message}
+          className={message.isUser ? 'right' : 'left'} // isUserプロパティでクラス名を切り替える
+        />
       ))}
-      {showAutoReply && (
-        <Message message={{ text: "Hello world!" }} className="left" />
-      )}
       {textMessages.map((message, index) => (
-        <div key={index} className={`text-message ${index % 2 === 0 ? 'right' : 'left'}`}>
-          <Message message={{ text: message }} className={index % 2 === 0 ? 'right' : 'left'} />
+        <div
+          key={index}
+          className={`text-message ${
+            message.isUser ? 'right' : 'left' // isUserプロパティでクラス名を切り替える
+          }`}
+        >
+          <Message
+            message={{ text: message.text }}
+            className={message.isUser ? 'right' : 'left'} // isUserプロパティでクラス名を切り替える
+          />
         </div>
       ))}
     </div>
