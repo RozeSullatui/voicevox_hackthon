6dc1aa9a70eb93f61dbb61330df72aec2301c96e
piyorin-hub <noripcode@gmail.com>
2023-04-14 10:16:51 +0000

    add Speech Recognition

--- voicevox-chat-front/src/components/Input.jsx
+++ voicevox-chat-front/src/components/Input.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { SpeechRecBtn } from './SpeechRecBtn';
 
 import '../styles.css'
 
@@ -13,9 +14,14 @@ export const ChatInput = ({ onSendMessage }) => {
     setText("");
   };
 
+  const handleRecognitionEnd = (recognizedText) => {
+    setText(recognizedText);
+  };
+
   const handleChange = (e) => {
     setText(e.target.value);
   };
+
 
   return (
     <form onSubmit={handleSubmit} className="chat-input">
@@ -25,6 +31,7 @@ export const ChatInput = ({ onSendMessage }) => {
         onChange={handleChange}
         placeholder="Type a message"
       />
+      <SpeechRecBtn onRecognitionEnd={handleRecognitionEnd}/>
       <button type="submit"><FontAwesomeIcon icon={faPaperPlane} /></button>
     </form>
   );
