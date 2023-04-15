0178a3221139fef546d3a4afd9281ef64e41a1ac
masatomo57 <akagawa.code@gmail.com>
2023-04-15 08:31:59 +0000

    前の会話を覚えておくようにchat_gpt.pyを変更し、main.pyを参考に置いておきます。

--- voicevox-chat-backend/chat/chat_gpt.py
+++ voicevox-chat-backend/chat/chat_gpt.py
@@ -8,19 +8,22 @@ load_dotenv(dotenv_path)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def callChatGPT(reply_text, role_text_file):
+def callChatGPT(reply_text, role_text_file, past_messages_list):
     
-    # role_text_fileをmessagesを読み込み
-    with open(role_text_file, 'r') as file:
-        dialogue = file.readlines()
-    
-    role_messages = []
-    message = ""
-    for line in dialogue:
-        message += line
-    
-    role_messages.append({"role": "system", "content": message})
-    role_messages.append({"role": "user", "content": reply_text})
+    # 最初の会話ならrole_text_fileをrole_messageに読み込んでpast_messages_listに追加
+    if len(past_messages_list) == 0:
+        role_message = ""
+        
+        with open(role_text_file, 'r') as file:
+            dialogue = file.readlines()
+        
+        for line in dialogue:
+                role_message += line
+                
+        past_messages_list.append({"role": "system", "content": role_message})
+        
+    # ユーザーからのリプライをpast_messages_listに追加
+    past_messages_list.append({"role": "user", "content": reply_text})
     
 
     completion = openai.ChatCompletion.create(
@@ -28,4 +31,4 @@ def callChatGPT(reply_text, role_text_file):
         messages=role_messages
         )
     # ChatGPTからの回答をreturn
-    return completion["choices"][0]["message"]["content"]
+    return completion["choices"][0]["message"]["content"], past_messages_list
