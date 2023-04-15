732d3a84120c3f0888d98a455e7dcecd7019444f
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-15 19:04:29 +0000

    charactor change

--- voicevox-chat-front/src/components/sideItem.jsx
+++ voicevox-chat-front/src/components/sideItem.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 import '../style/sidebar.css';
 
 
-export const SideItem = ({image, name}) => {
+export const SideItem = ({image, name, speaker_id=1}) => {
+
+    const handleChangeID = () => {
+
+        fetch('/changeCharacter', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            speaker_ID: speaker_id
+        })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const response = {speaker_ID: data.speaker_ID};
+            console.log(data.speaker_ID);
+            }
+        ).catch(error => console.error(error));
+    }
+
     return (
-        <div className="sidebar-row" >
+        <div className="sidebar-row" onClick={handleChangeID}>
         <img src={image} alt=''/>
         <h4>{name}</h4>
         </div>    
